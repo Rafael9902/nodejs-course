d8d9dad510b9551ad98b804114d778628a67f90f
Rafael Ortiz <rafaelortiz99@outlook.com>
2024-04-15 16:15:04 -0500

    http server

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { readFile, writeFile } from "node:fs/promises";
+import { createServer } from "node:http";
 
 const text = await readFile("input.txt", "utf-8");
 
@@ -7,3 +8,11 @@ const output = `Content: ${text} \nCreated on: ${Date.now()}`;
 await writeFile("output.txt", output);
 
 console.log("file writing");
+
+// SERVER
+
+const server = createServer((req, res) => {
+  res.end("Hello World");
+});
+
+server.listen(8000, "127.0.0.1", () => console.log("Listening at port 8000"));
